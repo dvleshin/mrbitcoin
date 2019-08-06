c1331e28d3513bf79de51b96d909d4068900fd3d
Dima Leshin <dmitryleshin@gmail.com>
2019-08-06 11:59:15 +0300

    Updates

---
+++ precache-manifest.5bfefca83558a3b56036b495080b718a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d174a83dd91e2106097339d1709dde7",
+    "url": "​https://dvleshin.github.io/mrbitcoin​/index.html"
+  },
+  {
+    "revision": "a8d31da380453d9f3177",
+    "url": "​https://dvleshin.github.io/mrbitcoin​/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "aaa45dcaedcb7c0dfc99",
+    "url": "​https://dvleshin.github.io/mrbitcoin​/static/css/main.1aac6380.chunk.css"
+  },
+  {
+    "revision": "a8d31da380453d9f3177",
+    "url": "​https://dvleshin.github.io/mrbitcoin​/static/js/2.22250cc8.chunk.js"
+  },
+  {
+    "revision": "aaa45dcaedcb7c0dfc99",
+    "url": "​https://dvleshin.github.io/mrbitcoin​/static/js/main.78e0c273.chunk.js"
+  },
+  {
+    "revision": "fe18d7fdb5184986a12c",
+    "url": "​https://dvleshin.github.io/mrbitcoin​/static/js/runtime~main.1ff5c568.js"
+  },
+  {
+    "revision": "5cdd1cc849030a90d4ae109e5216e215",
+    "url": "​https://dvleshin.github.io/mrbitcoin​/static/media/nouser.5cdd1cc8.png"
+  }
+]);
